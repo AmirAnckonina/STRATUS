6cddcd5bb94b410aa954a036b47904c31629e13c
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2023-05-04 14:49:49 +0300

    PromClient try

--- StratusApp/MonitoringClient/PrometheusClient.cs
+++ StratusApp/MonitoringClient/PrometheusClient.cs
@@ -1,9 +1,36 @@
-﻿using Prometheus;
+﻿//using Prometheus;
+using System.Net.Http;
+using Prometheus;
+using Prometheus.Client;
+using Prometheus.Client.Collectors;
+using static System.Net.WebRequestMethods;
 
 namespace MonitoringClient
 {
     public class PrometheusClient
     {
-       /// var server = new Prometheus.KestrelMetricServer()
+        private HttpClient _promHttpClient;
+
+        private MetricServer _metricServer;
+
+        public PrometheusClient()
+        {
+            string node_exporter_url = $"http://172.31.40.237:9100/";
+            /*_metricServer = new MetricServer(url: node_exporter_url, port: 9100); 
+            _metricServer.Start();*/
+            _promHttpClient= new HttpClient();
+            _promHttpClient.BaseAddress = new Uri(node_exporter_url);
+        }
+
+        public async Task<string> GetCpuUsage()
+        {
+            var response = await _promHttpClient.GetAsync("/metrics");
+            return await response.Content.ReadAsStringAsync();
+        }
+
+
+
+
+
     }
 }
---
+++ StratusApp/StratusApp/Controllers/PrometheusController.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.CloudWatch.Model;
+using Microsoft.AspNetCore.Mvc;
+using MonitoringClient;
+using StratusApp.Models.Responses;
+
+namespace StratusApp.Controllers
+{
+    public class PrometheusController : Controller
+    {
+        private readonly MonitoringClient.PrometheusClient _prometheusClient;
+
+        public PrometheusController()
+        {
+            _prometheusClient = new MonitoringClient.PrometheusClient(); 
+        }
+
+        [HttpGet("GetCPUUsage")]
+        public async Task<ActionResult<StratusResponse<List<Datapoint>>>> GetUserAwsInstanceData()
+        {
+            var userInstanceDataStartusResp = new StratusResponse<string>();
+
+            userInstanceDataStartusResp.Data = await _prometheusClient.GetCpuUsage();
+
+            return Ok(userInstanceDataStartusResp);
+        }
+    }
+}
