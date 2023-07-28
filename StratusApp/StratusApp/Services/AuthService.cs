cd3f26dd83d77bfbbfda089fc3f0585578d895d6
chenberger <chen10.berger@gmail.com>
2023-07-28 21:11:57 +0300

    started working on encryption, still needs to create our permanent key

---
+++ StratusApp/StratusApp/Services/AuthService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson;
+using StratusApp.Data;
+using StratusApp.Models;
+using StratusApp.Models.MongoDB;
+using StratusApp.Models.Responses;
+using StratusApp.Services.MongoDBServices;
+using System;
+using Utils.DTO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.IO;
+using Amazon.Runtime.Documents;
+using MongoDB.Driver;
+using BCrypt.Net;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StratusApp.Services
+{
+    public class AuthService
+    {
+        private readonly MongoDBService _mongoDatabase;
+        public AuthService(MongoDBService mongoDatabase)
+        {
+            _mongoDatabase = mongoDatabase;
+        }
+
+        internal Task<bool> IsUserExists(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task RegisterToStratusService(string username, string password, string accessKey, string secretKey)
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            string hashedAccessKey = 
+        }
+
+    }
+}
---
+++ StratusApp/Utils/EncryptionHelpers/EncryptionHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Utils.EncryptionHelpers
+{
+    public class EncryptionHelper
+    {
+        private static byte[] Key = Encoding.UTF8.GetBytes("your_secret_key_here"); // Replace with your secret encryption key.
+
+        public static string Encrypt(string plainText)
+        {
+            byte[] encrypted;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.GenerateIV();
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (var msEncrypt = new System.IO.MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (var swEncrypt = new System.IO.StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+
+            // Return the encrypted data as a base64-encoded string.
+            return Convert.ToBase64String(encrypted);
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            byte[] cipherText = Convert.FromBase64String(encryptedText);
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = cipherText.Take(16).ToArray();
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                using (var msDecrypt = new System.IO.MemoryStream(cipherText.Skip(16).ToArray()))
+                using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (var srDecrypt = new System.IO.StreamReader(csDecrypt))
+                {
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+    }
+}
